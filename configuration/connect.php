ecdba9e4fb804e864e427103de9013009d347c7e
Anderson Bastos <fbanderson.suporte@outlook.com.br>
2023-05-02 19:22:53 -0300

    trabalhando com a conexão e a criação do banco de dados

--- configuration/connect.php
+++ configuration/connect.php
@@ -1,1 +1,32 @@
-<!-- CONEXÃO COM BACO DE DADOS-->
+<?php
+
+define('HOST','localhost');
+define('DATABASENAME','fbanderson');
+define('USER','root');
+define('PASSWORD','');
+
+
+class Connect
+{
+    protected $connection;
+
+    function __construct()
+        {
+            $this->connectDatabase();
+        }
+
+    function connectDATABASE()
+        {
+            try {
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='. DATABASENAME, USER, PASSWORD);
+            } 
+            catch (PDOException $e) 
+            {
+            
+                echo "Error: ".$e->getMessage();
+                die();
+            }
+        }
+}
+    $testConnection = new Connect();
+?>
